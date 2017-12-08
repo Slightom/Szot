150b8ea651f93b8701d73da0e762adacadc2173b
Tomasz Suchwałko <tomasz.suchwalko@cssoftware.pl>
2017-12-08 16:34:18 +0100

    Add project files.

---
+++ Szot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Szot.Startup))]
+namespace Szot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
