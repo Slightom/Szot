2830db33ab05b9f8d1880ad439356b889c7c4d74
Tomasz Suchwałko <tomasz.suchwalko@cssoftware.pl>
2017-12-15 16:18:11 +0100

    dbsets made

--- Szot/Models/ApplicationDbContext.cs
+++ Szot/Models/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,17 @@ namespace Szot.Models
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Backing> Backings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<PlacePhoto> PlacePhotos { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongCategory> SongCategories { get; set; }
 
 
 
